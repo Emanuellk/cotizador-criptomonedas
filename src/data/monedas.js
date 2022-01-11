6d273eff981ec977cb621cd9920accda5dac8997
Emanuellk <emanuel.maggiolk@gmail.com>
2022-01-11 17:26:10 -0300

    Subiendo proyecto de criptomonedas

---
+++ src/data/monedas.js
@@ -0,0 +1,11 @@
+const monedas = [
+    { id:'USD', nombre:'Dólar Americano'},
+    { id:'ARG', nombre:'Peso Argentino'},
+    { id:'MXN', nombre:'Peso Mexicano'},
+    { id:'EUR', nombre:'Euro'},
+    { id:'GBP', nombre:'Libra esterlina'},
+]
+
+export {
+    monedas
+}
